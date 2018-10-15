dc9edf05301009eef04947f1494a523ac254e824
Talgorhythm <dia.99000@gmail.com>
2018-10-15 17:38:42 +0000

    implement chat slider

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -2,7 +2,6 @@
 *spa.shell.js
 *SPAのシェルモジュール
 */
-
 /*
 jslint				browser: true, 	continue: true,
 devel: true, 	indent: 2, 			maxerr: 50,
@@ -10,7 +9,6 @@ newcap: true, nomen: true, 		plusplus: true,
 regexp: true, sloppy: true, 	vars: false,
 white: true
 */
-
 /*global $, spa */
 spa.shell = (function(){
 	//----------------モジュールスコープ変数開始----------------
@@ -29,30 +27,101 @@ spa.shell = (function(){
 				+ '</div>'
 				+ '<div class="spa-shell-foot"></div>'
 				+ '<div class="spa-shell-chat"></div>'
-				+ '<div class="spa-shell-modal"></div>'
+				+ '<div class="spa-shell-modal"></div>',
+			//スライダーの格納と拡大の時間と高さを格納。
+			chat_extend_time: 1000,
+			chat_retract_time: 300,
+			chat_extend_height: 450,
+			chat_retract_height: 15,
+			chat_extended_title: 'Click to retract',
+			chat_retracted_title: 'Click to extend',
 		},
 		//モジュール全体で共有する動的情報をstateMapに配置
-		stateMap = {$container: null},
+		stateMap = {
+			$container: null,
+			is_chat_retracted: true,
+		},
 		//jqueryMapにjQueryコレクションをキャッシュ
 		jqueryMap = {},
 		//このセクションですべてのモジュールスコープ変数を宣言。後で割り当てる
-		setJqueryMap, initModule;
+		setJqueryMap, toggleChat, onClickChat, initModule;
 	//----------------モジュールスコープ変数終了----------------
 
 	//----------------ユーティリティメソッド開始----------------
 	//----------------ユーティリティメソッド終了----------------
 
 	//----------------DOMメソッド開始----------------
+	//DOMメソッド/setJqueryMap/開始
 	setJqueryMap = function(){
 		var $container = stateMap.$container;
 		jqueryMap = {
 			$container: $container,
-			$chat: $container.find( '.spa-shell-chat')
+			$chat: $container.find('.spa-shell-chat')
 		};
+	};
+	//DOMメソッド/setJqueryMap/終了
+	//DOMメソッド/toggleChat/開始
+	//目的：チャットスライダーの拡大や格納を行う
+	//引数：
+	// do_extend - trueの場合スライダーを拡大、falseの場合スライダーを格納
+	// callback - アニメーションの最後に実行するオプションの関数
+	//設定：
+	// chat_extend_time, chat_retract_time, chat_extend_height, chat_retract_height
+	//戻り値：boolean
+	// true - スライダーアニメーションが開始された
+	// false - スライダーアニメーションが開始されなかった
+	//状態：stateMap.is_chat_retractedを設定する
+	// true - スライダーが格納されている
+	// false - スライダーが拡大されている
+	//DOMメソッド/toggleChat/終了
+	toggleChat = function( do_extend, callback){
+		var
+			px_chat_ht = jqueryMap.$chat.height(),
+			is_open = px_chat_ht === configMap.chat_extend_height,
+			is_closed = px_chat_ht === configMap.chat_retract_height,
+			is_sliding = ! is_open && ! is_closed;
+
+		//競合状態を避ける
+		if(is_sliding){ return false;}
+
+		//チャットスライダーの拡大開始
+		if(do_extend){
+			jqueryMap.$chat.animate(
+				{height: configMap.chat_extend_height},
+				configMap.chat_extend_time,
+				function(){
+					jqueryMap.$chat.attr(
+						'title', configMap.chat_extended_title
+					);
+					stateMap.is_chat_retracted = false;
+					if(callback){callback(jqueryMap.$chat);}
+				}
+			);
+			return true;
+		}
+		//チャットスライダーの拡大終了
+		//チャットスライダーの格納開始
+		jqueryMap.$chat.animate(
+			{	height: configMap.chat_retract_height},
+			configMap.chat_retract_time,
+			function(){
+				jqueryMap.$chat.attr(
+					'title', configMap.chat_retracted_title
+				);
+				stateMap.is_chat_retracted = true;
+				if(callback){callback(jqueryMap.$chat);}
+			}
+		);
+		return true;
+		//チャットスライダーの格納終了
 	};
 	//----------------DOMメソッド終了----------------
 
 	//----------------イベントハンドラ開始----------------
+	onClickChat = function(event){
+		toggleChat(stateMap.is_chat_retracted);
+		return false;
+	};
 	//----------------イベントハンドラ終了----------------
 
 	//----------------パブリックメソッド開始----------------
@@ -61,6 +130,12 @@ spa.shell = (function(){
 		stateMap.$container = $container;
 		$container.html(configMap.main_html);
 		setJqueryMap();
+		stateMap.is_chat_retracted = true;
+		jqueryMap.$chat
+			.attr('title', configMap.chat_retracted_title)
+			.click(onClickChat);
+		// setTimeOut(function(){toggleChat(true);}, 3000);
+		// setTimeOut(function(){toggleChat(false);}, 8000);
 	};
 	return {initModule: initModule};
 	//----------------パブリックメソッド終了----------------
